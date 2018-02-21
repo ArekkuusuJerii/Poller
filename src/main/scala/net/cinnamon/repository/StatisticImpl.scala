ffa75f66933146788327f67df390e8c1d253faa7
ArekkuusuJerii <edgargcx99@gmail.com>
2018-02-20 23:24:50 -0600

    Two kinds of answers, those who do, and those who don't.

--- src/main/scala/net/cinnamon/repository/StatisticImpl.scala
+++ src/main/scala/net/cinnamon/repository/StatisticImpl.scala
@@ -8,21 +8,31 @@ import net.cinnamon.repository.helper.->
 import scala.collection.mutable.ArrayBuffer
 
 object StatisticImpl {
-  def getQuestionStatistics(token: Token, question: Question): ArrayBuffer[(String, Int)] = {
+  def getSelectionStatistics(token: Token, question: Question): ArrayBuffer[(String, Int)] = {
     val buf: ArrayBuffer[(String, Int)] = ArrayBuffer()
     val in = Map(
       "token" -> token,
       "question" -> question.id
     )
-    call(in, Map.empty)("{call getQuestionStatistics(?,?)}",
+    call(in, Map.empty)("{call getSelectionStatistics(?,?)}",
       _ => {
-        var answer: String = _
-        var amount: Int = _
+        var answer: String = ""
+        var amount: Int = 0
         ->(_, "answer")(answer = _)
         ->(_, "amount")(amount = _)
         buf +=((answer, amount))
       }
     )
+    buf
+  }
+
+  def getInputStatistics(token: Token, question: Question): ArrayBuffer[String] = {
+    val buf: ArrayBuffer[String] = ArrayBuffer()
+    val in = Map(
+      "token" -> token,
+      "question" -> question.id
+    )
+    call(in, Map.empty)("{call getInputStatistics(?,?)}", ->(_, "answer")(_ => buf += _))
     buf
   }
 }
